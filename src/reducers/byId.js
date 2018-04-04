b5f083bb8c711a6cc2a2843ccfced670d412d306
r00bal <frontendpit@gmail.com>
2018-04-04 07:47:18 +0200

    Add data to the fakeServer

--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -2,15 +2,20 @@ const byId = (state = {}, action) => {
     console.log('res',action);
   switch (action.type) {
     case 'FETCH_TODOS_SUCCESS':
-    const nextState = {...state }
+      const nextState = {...state }
 
-    action.response.forEach(todo => {
-      nextState[todo.id] = todo;
-    });
-    return nextState;
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
     default:
       return state;
-  }
+    case 'ADD_TODO_SUCCESS':
+      return {
+        ...state,
+        [action.response.id]: action.response,
+      }
+    }
 };
 
 export default byId;
